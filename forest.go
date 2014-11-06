2602d44caf379cc1eaf9fb3656aabfab696208e5
Michael Patraw <michaelpatraw@gmail.com>
2014-11-06 08:40:45 -0800

    NearbyLocation and FarawayLocation implemented.

--- forest.go
+++ forest.go
@@ -12,7 +12,7 @@ const (
 	// Chance to ascend deeper (closer to /). The weight
 	// has to be fair, because ascending is very limited
 	// and you only have one option.
-	AscentChance	= 0.35
+	AscendChance	= 0.35
 	
 	// Chance to move twice instead of once.
 	TwoStepChance	= 0.50
@@ -34,6 +34,7 @@ func newDirectoryForest() directoryForest {
 	}
 }
 
+// For a location to exist, the directory must exist.
 func (f *directoryForest) LocationExists(loc string) bool {
 	fi, err := os.Stat(loc)
 	if err != nil {
@@ -42,10 +43,50 @@ func (f *directoryForest) LocationExists(loc string) bool {
 	return fi.IsDir()
 }
 
+// Returns a location near the passed location. Nearby is
+// found by a small number of random directory changes. Will
+// not be the same directory, unless it has to (can't ascend
+// or descend).
+//
+// XXX: Currently this does not check if a rabbit already
+// exists at the new location. So we just lose rabbits
+// if one encounters another.
 func (f *directoryForest) NearbyLocation(loc string) string {
-	return ""
+	steps := 1
+	if chance(TwoStepChance) {
+		steps = 2
+	}
+	
+	for i := 0; i < steps; i++ {
+		// Can't move.
+		if !canAscend(loc) && !canDescend(loc) {
+			return loc
+		} else if chance(AscendChance) && canAscend(loc) {
+			loc = ascend(loc)
+		} else {
+			loc = randDescension(loc)
+		}
+	}
+	
+	return loc
+	
 }
 
+// A random faraway location. Rabbits typically start here
+// and run here when they're fleeing.
 func (f *directoryForest) FarawayLocation() string {
-	return ""
+	loc := baseLocation()
+	
+	steps := 1
+	if chance(TwoStepChance) {
+		steps = 2
+	}
+	
+	for i := 0; i < steps; i++ {
+		if canDescend(loc) {
+			loc = randDescension(loc)
+		}
+	}
+	
+	return loc
 }
