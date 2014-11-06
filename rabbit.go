90955991550c185b7c797b5eb47024cb3f63dd89
Michael Patraw <michaelpatraw@gmail.com>
2014-11-06 04:08:50 -0800

    Added a test file. Adding rabbit states and operations.

--- rabbit.go
+++ rabbit.go
@@ -3,25 +3,108 @@ package main
 
 import "time"
 
-type forest interface {
-
+// A forest is a place that can be traversed. Locations in a forest
+// are simple strings.
+type Forest interface {
+	// Returns true if passed location exists.
+	LocationExists(loc string) bool
+	// Returns a location fairly close to the one provided.
+	NearbyLocation(loc string) string
+	// Returns a faraway location, this could be anywhere.
+	FarawayLocation() string
 }
 
-type rabbit struct {
-	home		*forest
+type RabbitState int
+
+const (
+	// Initial state.
+	Wandering RabbitState = iota
+	// If the rabbit was successfully caught.
+	Caught
+	// If the rabbit died this will be the state. The rabbit only dies
+	// if the location it's in no longer exists.
+	Dead
+)
+
+// A rabbit is a simple creature that likes to move around a forest. You can
+// spot it, try to catch it, tag it, or accidentally kill it. :(
+type Rabbit struct {
+	// The forest the rabbit lives in.
+	home		Forest
+	// The current location in the forest.
 	location	string
+	// A tag identifying this specific rabbit.
 	tag		string
+	// The last location visited. May be "", in which case the
+	// rabbit never moved.
 	lastLocation	string
+	// The last time the rabbit moved to new location.
+	lastMoved	time.Time
+	// The time the rabbit was spotted last. May be nil.
 	lastSpotted	*time.Time
+	// State of the rabbit.
+	state		RabbitState
 }
 
-func newRabbit(f *forest) *rabbit {
-	return &rabbit{
-		f, "", "", "", nil,
+// Creates a new rabbit and moves it to a faraway location.
+func NewRabbit(f Forest) *Rabbit {
+	r := &Rabbit{
+		f, "", "", "", time.Now(), nil, Wandering,
+	}
+	r.location = f.FarawayLocation()
+	return r
+}
+
+// The rabbit wakes up and and decides if it wants to move.
+func (r *Rabbit) Wakeup() {
+	if !r.IsWandering() {
+		return
+	}
+	if !r.LocationExists(r.location) {
+		r.state = Dead
+		return
 	}
 }
 
-func (r *rabbit) spot() {
+// The rabbit was spotted, make it flee.
+func (r *Rabbit) Spot() {
 	t := time.Now()
 	r.lastSpotted = &t
+
+}
+
+func (r *Rabbit) TryCatch() {
+
+}
+
+func (r *Rabbit) TryTag() {
+
+}
+
+func (r *Rabbit) Location() string {
+	return r.location
+}
+
+func (r *Rabbit) Tag() string {
+	return r.tag
+}
+
+func (r *Rabbit) WasSpotted() bool {
+	return r.lastSpotted != nil
+}
+
+func (r *Rabbit) State() RabbitState {
+	return r.state
+}
+
+func (r *Rabbit) IsWandering() bool {
+	return r.state == Wandering
+}
+
+func (r *Rabbit) IsCaught() bool {
+	return r.state == Caught
+}
+
+func (r *Rabbit) IsDead() bool {
+	return r.state == Dead
 }
---
+++ test_rabbit.go
@@ -0,0 +1,8 @@
+
+package main
+
+import "testing"
+
+func TestMoving(t *testing.T) {
+
+}
